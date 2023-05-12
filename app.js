caa9c1ad6ec5609b4ec55dd7785b02ac3ec3f185
Bryan Matias <30980194+joshuarivarez@users.noreply.github.com>
2023-05-12 15:31:38 +0800

    NodeJS API

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const apiRoutes = require('./routes/api.routes');
+
+const app = express();
+
+require('./config/db');
+
+app.use(cors());
+app.use(
+  express.urlencoded({extended: true})
+);
+
+app.use(express.json());
+app.use('/api', apiRoutes);
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () =>{
+  console.log(`Server is running in port ${port}`);
+})
